ae88e79c067a91f5c5b08c408aa3fc12c3c4789f
Suelen Dias <60116445+suelendias@users.noreply.github.com>
2022-05-12 13:20:11 -0300

    Update multiplicacaoSimples.java

--- DesafiosIniciais/multiplicacaoSimples.java
+++ DesafiosIniciais/multiplicacaoSimples.java
@@ -1,7 +1,8 @@
 /*
     > DESAFIO
 
-    Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta de acordo com a mensagem de saída esperada (exemplo abaixo).
+    Você receberá dois valores inteiros. Faça a leitura e em seguida calcule o produto entre estes dois valores. Atribua esta operação à variável PROD, mostrando esta 
+    de acordo com a mensagem de saída esperada (exemplo abaixo).
 
     Entrada
     A entrada contém 2 valores inteiros.
